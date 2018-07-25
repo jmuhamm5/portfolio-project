dbfe26c4ea069701c0b5dfe26f1d46d4bd52fc2d
Jamil Muhammad <apple_life15@yahoo.com>
2018-07-25 05:47:40 -0500

    Added local support

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'resumedb',
         'USER': 'postgres',
-        'PASSWORD': 'blackfirst123',
+        'PASSWORD': 'django123',
         'HOST': 'localhost',
         'PORT': '5432'
     }
@@ -132,3 +132,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
